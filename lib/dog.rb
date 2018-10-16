321eef2727bddb256fc0696009b8b9e6ddb36cf8
Karl Benbeneck <kbenbeneck@gmail.com>
2018-10-16 01:12:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,25 +1,15 @@
 class Dog
-  def initalize(name)
+
+  def initialize(name, breed="Mutt")
     @name = name
+    @breed = breed
   end
 
   def name
     @name
   end
 
-  def name=(new_name)
-    @name = new_name
-  end
-
-  def initalize(breed)
-     @breed = breed
-  end
-
   def breed
     @breed
-  end
-
-  def breed=(new_breed)
-    @breed = new_breed
   end
 end
